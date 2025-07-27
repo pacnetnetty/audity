edab3f10d50c14b5cdde2fceb28fcb490cfb3c09
Pacnet <bhegde8@gmail.com>
2025-07-27 07:22:23 -0400

    chore: update api service boilerplate

--- packages/backend/api-service/src/test/java/com/audity/api_service/ApiServiceApplicationTests.java
+++ packages/backend/api-service/src/test/java/com/audity/api_service/ApiServiceApplicationTests.java
@@ -1,11 +1,13 @@
+package com.audity.api_service;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class ApiServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
